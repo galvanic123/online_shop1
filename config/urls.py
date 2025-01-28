bcef7ffee1f31c4f452770177224fcd6f366162e
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-01-28 23:34:34 +0300

    midl1

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from catalog.views import home, contacts
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path('', home),
     path("catalog/", include("catalog.urls", namespace='catalog')),
 ]
