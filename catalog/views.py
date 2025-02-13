9c81ade4f9ae1b34ea00ad8a2ccf1e57c6f8b74f
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-02-14 00:13:45 +0300

    init 24.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, "catalog/home.html")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "catalog/base.html", context=context)
 
 
 def contacts(request):
@@ -15,3 +19,9 @@ def contacts(request):
         # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, "catalog/contacts.html")
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    context = {"product": product}
+    return render(request, "catalog/product_detail.html", context=context)
