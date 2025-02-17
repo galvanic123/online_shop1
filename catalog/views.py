c391ea9e38b38aaed793e8f908f071dd612c9f4f
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-02-17 23:59:09 +0300

    init 25.1

--- catalog/views.py
+++ catalog/views.py
@@ -1,33 +1,58 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
+class CatalogHomeView(ListView):
+    model = Product
+    template_name = 'catalog/base.html'
+    context_object_name = 'products'
 
-def home(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "catalog/base.html", context=context)
 
+class CatalogContactsView(View):
+    def get(self, request):
+        return render(request, 'catalog/contacts.html')
 
-def contacts(request):
-    if request.method == "POST":
-        # Получение данных из формы
-        name = request.POST.get("name")
-        message = request.POST.get("message")
-        # Обработка данных
+    def post(self, request):
+        #Получение данных из формы
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
         # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, "catalog/contacts.html")
 
 
-def product_detail(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    context = {"product": product}
-    return render(request, "catalog/product_detail.html", context=context)
+class CatalogDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
 
-
-def product_list(request):
-    products = Product.objects.all()
-    context = {"product": products}
-    return render(request, "catalog/product_list.html", context=context)
+# def home(request):
+#     products = Product.objects.all()
+#     context = {"products": products}
+#     return render(request, "catalog/base.html", context=context)
+#
+#
+# def contacts(request):
+#     if request.method == "POST":
+#         # Получение данных из формы
+#         name = request.POST.get("name")
+#         message = request.POST.get("message")
+#         # Обработка данных
+#         # Здесь мы просто возвращаем простой ответ
+#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+#     return render(request, "catalog/contacts.html")
+#
+#
+# def product_detail(request, product_id):
+#     product = get_object_or_404(Product, id=product_id)
+#     context = {"product": product}
+#     return render(request, "catalog/product_detail.html", context=context)
+#
+#
+# def product_list(request):
+#     products = Product.objects.all()
+#     context = {"product": products}
+#     return render(request, "catalog/product_list.html", context=context)
