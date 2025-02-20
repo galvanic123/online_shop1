038fdfe4b0799b930bfa44ad6bbeba82fbb16efb
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-02-20 22:12:53 +0300

    init 25.6

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': [],  # Главная директория с шаблонами
+        'DIRS': [BASE_DIR / 'catalog/templates'],  # Главная директория с шаблонами
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
