0a7ac3e93a6db96035ad807e6a2d59de035f8079
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-03-10 22:12:12 +0300

    init 27 2

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,7 @@
 import os
+
+from django.conf.global_settings import AUTH_USER_MODEL
+from django.db.models import AutoField
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -126,3 +129,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+
+    phone = models.CharField(max_length=35, verbose_name="Телефон", blank=True, null=True, help_text="Введите номер телефона")
+    avatar = models.ImageField(upload_to='users/avatars', blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
+
